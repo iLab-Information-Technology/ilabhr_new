49f97ac818d68b18d7cee01b949abe4968652e08
= <=>
2024-06-02 15:25:35 +0500

    Business Report Issue Fixed

--- resources/views/employees/ajax/create.blade.php
+++ resources/views/employees/ajax/create.blade.php
@@ -334,6 +334,18 @@ $addDesignationPermission = user()->permission('add_designation');
     <script src="{{ asset('vendor/jquery/clipboard.min.js') }}"></script>
 @endif
 <script>
+    function generatePassword(fieldId) {
+        var length = 8,
+            charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+            password = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+            password += charset.charAt(Math.floor(Math.random() * n));
+        }
+        document.getElementById(fieldId).value = password;
+    }
+</script>
+<script>
+
     $(document).ready(function() {
 
         $('.custom-date-picker').each(function(ind, el) {
