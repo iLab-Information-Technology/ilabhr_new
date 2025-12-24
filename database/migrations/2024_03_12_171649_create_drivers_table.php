dfebbeb757b4c30de64dc65cbdea8ae8b0c869df
Usman Asif <134294024+itdilab@users.noreply.github.com>
2025-12-24 15:01:24 +0300

    Update 2024_03_12_171649_create_drivers_table.php

--- database/migrations/2024_03_12_171649_create_drivers_table.php
+++ database/migrations/2024_03_12_171649_create_drivers_table.php
@@ -11,29 +11,31 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('drivers', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('company_id')->unsigned()->nullable();
-            $table->integer('nationality_id')->unsigned()->nullable();
-            $table->string('name', 155);
-            $table->string('iqaama_number');
-            $table->string('absher_number');
-            $table->string('sponsorship');
-            $table->string('sponsorship_id');
-            $table->string('insurance_policy_number');
-            $table->string('address')->nullable();
-            $table->text('remarks')->nullable();
-            $table->string('email');
-            $table->string('work_mobile_no');
-            $table->date('insurance_expiry_date');
-            $table->date('license_expiry_date');
-            $table->date('iqaama_expiry_date');
-            $table->date('date_of_birth');
+        if (!Schema::hasTable('drivers')) {
+            Schema::create('drivers', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('company_id')->unsigned()->nullable();
+                $table->integer('nationality_id')->unsigned()->nullable();
+                $table->string('name', 155);
+                $table->string('iqaama_number');
+                $table->string('absher_number');
+                $table->string('sponsorship');
+                $table->string('sponsorship_id');
+                $table->string('insurance_policy_number');
+                $table->string('address')->nullable();
+                $table->text('remarks')->nullable();
+                $table->string('email');
+                $table->string('work_mobile_no');
+                $table->date('insurance_expiry_date');
+                $table->date('license_expiry_date');
+                $table->date('iqaama_expiry_date');
+                $table->date('date_of_birth');
 
-            $table->foreign('nationality_id')->references('id')->on('countries')->onUpdate('CASCADE')->onDelete('CASCADE');
-            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade')->onUpdate('cascade');
-            $table->timestamps();
-        });
+                $table->foreign('nationality_id')->references('id')->on('countries')->onUpdate('CASCADE')->onDelete('CASCADE');
+                $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade')->onUpdate('cascade');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
